166aa42e4f11e93c4a946ad5ebc197edef102ee3
Nuwan Chandima <nuwanchandima71@gmail.com>
2025-11-13 12:06:23 +0530

    Change server port from 8000 to 5000 for application launch

--- main.py
+++ main.py
@@ -542,4 +542,4 @@ async def process_video(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
